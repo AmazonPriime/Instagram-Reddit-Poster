cfe21681b1696ebedd17fc37d7051e86e90820cd
Luke <2342915h@student.gla.ac.uk>
2022-07-13 03:09:22 +0100

    Added code for the Reddit class and added some code which will grab submissions to store in json

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import argparse
+import json
+import os
+from reddit import Reddit
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,10 +10,33 @@ parser = argparse.ArgumentParser(
     description='Tool to gather and post memes to instagram.')
 
 parser.add_argument('--post', action='store_true',
-                    help='Select meme from the folder and post to instagram.')
+                    help='Select random meme from list and post to instagram.')
 
-parser.add_argument('-n', metavar='int', default=30,
+parser.add_argument('-n', '--number', default=48, type=int,
                     help='Number of memes to gather into memes folder.')
+
+parser.add_argument('-subs', '--subreddits', nargs='+', default=['memes'],
+                    help='List of the different subreddits to be searched.')
+
+
+def store_json(data, path):
+    if type(data) == list or type(data) == dict:
+        path = os.path.join(os.getcwd(), path)
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
+
+    if not args.post:
+        reddit = Reddit()
+        if not reddit.load(path='Reddit_Meme_Getter/reddit.pickle'):
+            reddit.new(
+                os.getenv('REDDIT_CID'),
+                os.getenv('REDDIT_SECRET'),
+                os.getenv('REDDIT_PASSWORD'),
+                os.getenv('REDDIT_USERNAME'),
+            )
+        submissions = reddit.get_submissions(args.subreddits, args.number)
+        store_json(submissions, 'memes.json')
--- reddit.py
+++ reddit.py
@@ -1,1 +1,50 @@
 # handles the logic for gathering the memes and picking them
+import pickle
+import praw
+import os
+
+
+class Reddit:
+    def __init__(self):
+        self.reddit = None
+
+    def new(self, cid, secret, user, password):
+        self.reddit = praw.Reddit(
+            client_id=cid,
+            client_secret=secret,
+            password=password,
+            username=user,
+            user_agent='Memescript by u/' + user
+        )
+
+    def load(self, path='reddit.pkl'):
+        fullpath = os.path.join(os.getcwd(), path)
+        if os.path.exists(fullpath):
+            with open(fullpath, 'rb') as f:
+                self.reddit = pickle.load(f)
+                return True
+        print('Could not load reddit instance from path: ' + path)
+        return False
+
+    def save(self, path='reddit.pkl'):
+        fullpath = os.path.join(os.getcwd(), path)
+        with open(fullpath, 'wb') as f:
+            pickle.dump(self.reddit, f)
+
+    def get_submissions(self, subreddits, number, filter='day'):
+        subreddits = '+'.join(subreddits)
+        submissions_generator = self.reddit.subreddit(
+            subreddits).top(limit=number, time_filter=filter)
+        submissions = []
+        for submission in submissions_generator:
+            if not submission.over_18:
+                submissions.append({
+                    'id': submission.id,
+                    'author': submission.author.name,
+                    'title': submission.title,
+                    'upvotes': submission.score,
+                    'upvote_ratio': submission.upvote_ratio,
+                    'url': submission.url,
+                    'permalink': submission.permalink
+                })
+        return submissions
